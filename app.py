2a599600fb4ad52fa8281441f105076c1ada94bc
Luis Carpi <luiscarpi1989@gmail.com>
2024-06-07 13:19:02 +0300

    Added a folderinput and dataoutput

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
+from flask import Flask, request, redirect, url_for, render_template
 import os
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
-
 app = Flask(__name__)
-
+UPLOAD_FOLDER = 'static/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        files = request.files.getlist('file')
+        for file in files:
+            if file:
+                filename = file.filename
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('display_images'))
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+@app.route('/display')
+def display_images():
+    images = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('display.html', images=images)
 
 if __name__ == '__main__':
-   app.run()
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    app.run(debug=True)
