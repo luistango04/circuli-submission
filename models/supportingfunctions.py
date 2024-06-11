c39997a63d4a589769bfa6cd2aa1c589733d242a
Luis Carpi <luiscarpi1989@gmail.com>
2024-06-11 18:29:37 +0300

    Circu-li - ion

--- models/supportingfunctions.py
+++ models/supportingfunctions.py
@@ -41,12 +41,6 @@ def jsonprepare(screws,transformation_matrix):
 
 def displaypointclouds(od3object):
     filename = 'static/results.ply'
-    # Visualize the first half point cloud
-    # o3d.visualization.draw_geometries([od3object],
-    #                                   zoom=0.3412,
-    #                                   front=[0.4257, -0.2125, -0.8795],
-    #                                   lookat=[2.6172, 2.0475, 1.532],
-    #                                   up=[-0.0694, -0.9768, 0.2024])
 
     o3d.io.write_point_cloud(filename, od3object)
     return filename
