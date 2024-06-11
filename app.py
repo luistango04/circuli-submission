78f481187dac65b0da2db3abaab177bd316749a9
Luis Carpi <luiscarpi1989@gmail.com>
2024-06-11 15:18:35 +0300

    Circu-li - ion

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ file_container = FileContainer()
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+#
 @app.route('/')
 def index():
     return render_template('index.html')
